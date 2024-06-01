62c94fdcbefa6f3a03a9049c038200ebaff9bb3b
a-KaiSiang <khorkaisiang01@gmail.com>
2024-06-02 01:27:19 +0800

    login page done

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import Login from './page/Login';
 import './App.css';
 
 function App() {
+  const pageContainer = {
+    // 'width': '100vw',
+    'height' : '100vh',
+    'backgroundColor' : 'skyblue',
+    // 'boxsizing' : 'border-box'
+    // 'minWidth' : '1000px',
+  }
+
   return (
-    <div>
+    <div style={pageContainer}>
       <Login />
     </div>
   );
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 import '../css/Login.css';
+
 export default function Login(){
+    //style
+    const inputField = {
+        'display' : 'flex',
+    }
+
+    const inputHeight = {
+        'height' : '2em'
+    }
+
+    //state
+    const [userCredential, setUserCredential] = useState({
+        username : '',
+        password : ''
+    })
+
     return(
-        <div>
-            <h3>Budget Management Appdddd</h3>
+        <div className="login_page">
+            <h2 className="header">Budget App Managemenet</h2>
+
+            <div style={inputField}>
+                <div className="input_elems input_label">
+                    <label htmlFor='username'>Username  <br></br></label>
+                    <label htmlFor='password'>Password  </label>
+                </div>
+                
+                <div className="input_elems"> 
+                    <input id='username' size="40" style={inputHeight} value={userCredential.username} 
+                    onChange={e=>{
+                        setUserCredential({
+                            ...userCredential,
+                            username:e.target.value
+                        })
+                    }}>
+
+                    </input>
+                    <input id='password' size="40" style={inputHeight} value={userCredential.password} 
+                    onChange={e=>{
+                        setUserCredential({
+                            ...userCredential,
+                            password:e.target.value
+                        })
+                    }}>
+
+                    </input>
+                </div>
+               
+            </div>
+
+            <button type="submit" className="button-81" onClick={()=>console.log(userCredential)}>Login</button>
         </div>
     )
 }
