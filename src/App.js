115606a563cf9c0d9d301f89ba8d8b65ae854fe7
a-KaiSiang <khorkaisiang01@gmail.com>
2024-06-01 17:27:02 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Login from './page/Login';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import '../css/Login.css';
+export default function Login(){
+    return(
+        <div>
+            <h3>Budget Management Appdddd</h3>
+        </div>
+    )
+}
